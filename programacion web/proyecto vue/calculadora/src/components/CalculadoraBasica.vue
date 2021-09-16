6f57d77c3fa56ea495d75830332fce3c07395db8
RAMIVAR <RAMIVAR11@HOTMAIL.COM>
2021-09-16 16:54:11 -0500

    proyecto

---
+++ programacion web/proyecto vue/calculadora/src/components/CalculadoraBasica.vue
@@ -0,0 +1,102 @@
+<template>
+  <div>
+    <titulo />
+    <v-row>
+      <v-col class="text-center">
+        <h1>Calculadora Básica</h1>
+      </v-col>
+    </v-row>
+    <v-row>
+        <v-col>
+            <v-text-field
+            type="number"
+            label="Número 1"
+            :rules="rules"
+            hide-details="auto"
+            v-model="n1"
+            ></v-text-field>
+        </v-col>
+
+        <v-col>
+            <v-text-field
+            type="number"
+            label="Número 2"
+            :rules="rules"
+            hide-details="auto"
+            v-model="n2"
+            ></v-text-field>
+        </v-col>
+    </v-row>
+    
+    <br />
+
+    <v-row class="text-center">
+      <v-col>
+        <v-btn color="primary" elevation="15" rounded @click="sumar()"
+          >sumar</v-btn
+        >
+      </v-col>
+      <v-col>
+        <v-btn color="primary" elevation="15" rounded @click="restar()"
+        >Restar</v-btn
+        >
+      </v-col>
+      <v-col>
+        <v-btn color="primary" elevation="15" rounded @click="multiplicar()"
+          >Multiplicar</v-btn
+        >
+      </v-col>
+      <v-col>
+        <v-btn color="primary" elevation="15" rounded @click="dividir()"
+          >Dividir</v-btn
+        >
+      </v-col>
+    </v-row>
+
+    <br />
+    {{ resp }}
+  </div>
+</template>
+
+<script>
+import titulo from "./titulo.vue";
+
+export default {
+  components: { titulo },
+
+  name: "CalculadoraBasica",
+  data: () => {
+    return {
+      n1: null,
+      n2: null,
+      resp: "",
+      rules: [
+        value => !!value || 'Requerido.',
+        value => (value && value.length >= 2) || 'Min 2 caracteres',
+      ],
+    };
+  },
+
+  methods: {
+    sumar() {
+      this.resp = parseFloat(this.n1) + parseFloat(this.n2);
+    },
+    restar() {
+      this.resp = parseFloat(this.n1) - parseFloat(this.n2);
+    },
+    multiplicar() {
+      this.resp = parseFloat(this.n1) * parseFloat(this.n2);
+    },
+    dividir() {
+      if (this.n2 == 0) {
+        alert("Not a number");
+      } else {
+        this.resp = parseFloat(this.n1) / parseFloat(this.n2);
+      }
+    },
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ programacion web/proyecto vue/calculadora/src/components/titulo.vue
@@ -0,0 +1,21 @@
+<template>
+<v-container>
+    <p class = "text-center">
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias ipsa assumenda maiores dolor in velit non aperiam, debitis fugit veniam distinctio cum, possimus soluta ea. Reprehenderit, ut obcaecati. Dignissimos, velit!
+    <br />
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint quasi, officia, dolores enim, blanditiis fugiat fugit facere officiis consequuntur corporis fuga dolorem esse voluptatum eveniet quidem mollitia dicta similique accusamus!
+    </p>
+
+</v-container>
+
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
